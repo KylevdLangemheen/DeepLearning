fc1babad080039535812b98a8ad0844dffa64ff2
KylevdLangemheen <kylevdlangemheen@live.nl>
2021-02-24 23:21:24 +0100

    added Optuna + flake8 compliant

--- resnet.py
+++ resnet.py
@@ -3,15 +3,7 @@ from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization,\
                                     Add, AveragePooling2D, Flatten, Dense
 from tensorflow.keras.models import Model
 from keras.datasets import fashion_mnist
-from keras.applications.resnet import preprocess_input
 from keras.utils import to_categorical
-from keras.layers import Flatten, Dense
-from keras.callbacks import EarlyStopping
-from keras import Model
-# import matplotlib.pyplot as plt
-import numpy as np
-from PIL import Image
-import keras
 
 
 def relu_bn(inputs: Tensor) -> Tensor:
@@ -19,9 +11,11 @@ def relu_bn(inputs: Tensor) -> Tensor:
     bn = BatchNormalization()(relu)
     return bn
 
-def residual_block(x: Tensor, downsample: bool, filters: int, kernel_size: int = 3) -> Tensor:
+
+def residual_block(x: Tensor, downsample: bool, filters: int,
+                   kernel_size: int = 3) -> Tensor:
     y = Conv2D(kernel_size=kernel_size,
-               strides= (1 if not downsample else 2),
+               strides=(1 if not downsample else 2),
                filters=filters,
                padding="same")(x)
     y = relu_bn(y)
@@ -39,29 +33,34 @@ def residual_block(x: Tensor, downsample: bool, filters: int, kernel_size: int =
     out = relu_bn(out)
     return out
 
+
 def create_res_net():
-    
+
     inputs = Input(shape=(28, 28, 1))
     num_filters = 64
-    
+
     t = BatchNormalization()(inputs)
     t = Conv2D(kernel_size=3,
                strides=1,
                filters=num_filters,
                padding="same")(t)
     t = relu_bn(t)
-    
+
     num_blocks_list = [2, 5, 5, 2]
     for i in range(len(num_blocks_list)):
         num_blocks = num_blocks_list[i]
         for j in range(num_blocks):
-            t = residual_block(t, downsample=(j==0 and i!=0), filters=num_filters)
+            t = residual_block(
+                t,
+                downsample=(j == 0 and i != 0),
+                filters=num_filters
+            )
         num_filters *= 2
-    
+
     t = AveragePooling2D(4)(t)
     t = Flatten()(t)
     outputs = Dense(10, activation='softmax')(t)
-    
+
     model = Model(inputs, outputs)
 
     model.compile(
@@ -72,14 +71,17 @@ def create_res_net():
 
     return model
 
+
 def main():
     model = create_res_net()
     train, test = fashion_mnist.load_data()
 
     (train_images, train_labels), (test_images, test_labels) = train, test
 
+    '''
     class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    '''
 
     # Normalize images
     train_images = train_images / 255.0
@@ -93,28 +95,20 @@ def main():
     train_images = train_images.reshape(-1, 28, 28, 1)
     test_images = test_images.reshape(-1, 28, 28, 1)
 
-    callbacks = [keras.callbacks.EarlyStopping(
-    monitor='val_loss',
-    patience=3,
-    verbose=1, mode='auto'
-    )]
-
     # train model on training set
     model.fit(
         train_images,
         train_labels_onehot,
         epochs=50,
         validation_split=0.2,
+        batch_size=128,
         verbose=1
     )
-
 
     # test trained model
     test_loss, test_acc = model.evaluate(test_images, test_labels_onehot)
     print('Test loss', test_loss)
     print('Test accuracy', test_acc)
-
-
 
 
 if __name__ == '__main__':
---
+++ vggnetwork_optimized.py
@@ -0,0 +1,115 @@
+import keras
+from keras.datasets import fashion_mnist
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+from keras.models import Sequential
+from keras.utils import to_categorical
+import optuna
+import joblib
+
+
+# load all the data
+# fashion_mnist = keras.datasets.fashion_mnist
+train, test = fashion_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = train, test
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+# Normalize images
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# convert name labels into form [0,0,0,1]
+train_labels_one_hot = to_categorical(train_labels)
+test_labels_one_hot = to_categorical(test_labels)
+
+# reshape them since its black/white so last channel is 1 (RGB is 3)
+train_images = train_images.reshape(-1, 28, 28, 1)
+test_images = test_images.reshape(-1, 28, 28, 1)
+
+
+# lrelu = lambda x: keras.activations.relu(x, alpha=0.1)
+def lrelu(x): return keras.activations.relu(x, alpha=0.1)
+
+
+def objective(trial):
+    optimizer = trial.suggest_categorical('optimizer', [
+        'SGD',
+        'RMSprop',
+        'Adam',
+        'Adadelta',
+        'Adagrad',
+        'Adamax',
+        'Nadam',
+        'Ftrl'
+    ])
+    dropout = trial.suggest_float('dropout', 0, 1)
+    # l1 = trial.suggest_loguniform('l1', 0.000001, 0.1)
+
+    # create model
+    model = Sequential()
+
+    model.add(Conv2D(
+        32,
+        (3, 3),
+        strides=1,
+        padding="same",
+        activation=lrelu,
+        input_shape=(28, 28, 1)
+    ))
+    model.add(keras.layers.BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Conv2D(32, (3, 3), strides=1, padding="same", activation=lrelu))
+    model.add(keras.layers.BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(64, (3, 3), strides=1, padding="same", activation=lrelu))
+    model.add(keras.layers.BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Conv2D(64, (3, 3), strides=1, padding="same", activation=lrelu))
+    model.add(keras.layers.BatchNormalization())
+
+    model.add(Dropout(dropout))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Conv2D(128, (3, 3), strides=1, padding="same", activation=lrelu))
+    model.add(keras.layers.BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(Conv2D(128, (3, 3), strides=1, padding="same", activation=lrelu))
+    model.add(keras.layers.BatchNormalization())
+    model.add(Dropout(dropout))
+    model.add(MaxPooling2D((2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(10, activation="softmax"))
+
+    model.compile(
+        optimizer=optimizer,
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    # train model on training set
+    model.fit(
+        train_images,
+        train_labels_one_hot,
+        epochs=10,
+        validation_split=0.2,
+        batch_size=128,
+        verbose=1
+    )
+
+    # test trained model
+    test_loss, test_acc = model.evaluate(test_images, test_labels_one_hot)
+    print('Test loss', test_loss)
+    print('Test accuracy', test_acc)
+    return test_acc
+
+
+study = optuna.create_study(direction='maximize')
+study.optimize(objective, n_trials=3)
+
+print(study.best_params)
+joblib.dump(study, 'study.pkl')
